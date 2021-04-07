8d0d9f975722de2bde12165a6b597994de0ef4c5
Sebastian Tovar <sctovarp@unal.edu.co>
2021-04-07 14:01:17 -0500

    first method done

--- main.go
+++ main.go
@@ -15,32 +15,58 @@ func main() {
 	// 	{8, 9, 10, 11},
 	// }
 	// Generate a 6×6 matrix of random values.
-	data := make([]float64, 12)
-	var s = -1
-	for i := range data {
-		s = s + 1
-		data[i] = float64(s)
-	}
-	a := mat.NewDense(3, 4, data)
-	matPrint(a)
+	// data := make([]float64, 12)
+	// var s = -1
+	// for i := range data {
+	// 	s = s + 1
+	// 	data[i] = float64(s)
+	// }
+	data1 := []float64{1, 3, 5, 7, 11, 13}
+	a_prim := mat.NewDense(3, 2, data1)
+	// a := mat.NewDense(3, 4, data)
+	// matPrint(a)
+	matPrint(a_prim)
 
-	b := a.T()
-	matPrint(b)
+	b_prim := a_prim.T()
+	matPrint(b_prim)
 
-	mul1 := mat.NewDense(3, 3, nil)
-	mul1.Mul(a, b)
+	mul1_prim := mat.NewDense(2, 2, nil)
+	mul1_prim.Mul(b_prim, a_prim)
 	fmt.Println("multiplicacion:")
-	matPrint(mul1)
+	matPrint(mul1_prim)
 
-	mul2 := mat.NewDense(3, 3, nil)
-	mul2.Inverse(mul1)
-	matPrint(mul1)
+	inver_prim := mat.NewDense(2, 2, nil)
+	inver_prim.Inverse(mul1_prim)
+	fmt.Println("inversa:")
+	matPrint(inver_prim)
 
-	fmt.Println(mul2.Dims())
-	fmt.Println(b.Dims())
-	final := mat.NewDense(4, 4, nil)
-	final.Mul(mul2, b)
-	matPrint(final)
+	final_matrix := mat.NewDense(2, 3, nil)
+	final_matrix.Mul(inver_prim, b_prim)
+	fmt.Println("final matrix:")
+	matPrint(final_matrix)
+
+	checker := mat.NewDense(2, 2, nil)
+	checker.Mul(final_matrix, a_prim)
+	fmt.Println("checker:")
+	matPrint(checker)
+
+	// b := a.T()
+	// matPrint(b)
+
+	// mul1 := mat.NewDense(3, 3, nil)
+	// mul1.Mul(a, b)
+	// fmt.Println("multiplicacion:")
+	// matPrint(mul1)
+
+	// mul2 := mat.NewDense(3, 3, nil)
+	// mul2.Inverse(mul1)
+	// matPrint(mul1)
+
+	// fmt.Println(mul2.Dims())
+	// fmt.Println(b.Dims())
+	// final := mat.NewDense(4, 4, nil)
+	// final.Mul(mul2, b)
+	// matPrint(final)
 
 	// var i, j int
 	// for i = 0; i < 3; i++ {
